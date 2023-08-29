fe1acdb7f3544b1df67c03d87ae06db4f17524ab
Tan Tran <duytantran.it@gmail.com>
2023-08-29 10:19:21 +0700

    Create sample about access elements in DOM

---
+++ samples/additional-DOM/index.js
@@ -0,0 +1,16 @@
+const button = document.getElementById('changeBackground');
+button.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'cyan';
+});
+
+const demoClass = document.getElementsByClassName('demo-class');
+console.log(demoClass);
+for (i = 0; i <= demoClass.length; i++) {
+  demoClass[i].style.color = 'red';
+}
+
+const demoQuery = document.querySelectorAll('.demo-query-all');
+console.log(demoQuery);
+demoQuery.forEach((query) => {
+  query.style.color = 'blue';
+});
