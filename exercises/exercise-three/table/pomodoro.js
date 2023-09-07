76f08d74932e2cbc9de0d71f29a6eafe6fb5a313
Tan Tran <duytantran.it@gmail.com>
2023-09-07 17:14:06 +0700

    Update naming convention and comments

--- exercises/exercise-three/table/pomodoro.js
+++ exercises/exercise-three/table/pomodoro.js
@@ -3,7 +3,7 @@ let tasks = [];
 const pomodoroForm = document.querySelector('.js-add-task');
 const pomodoroTableBody = document.querySelector('.js-task-table-body');
 
-// Function add new tasks to table body
+// Function add new Tasks to Table-Body
 const addTask = function(e) {
   e.preventDefault();
 
@@ -14,7 +14,7 @@ const addTask = function(e) {
 	// Reset form field
 	this.reset();
 
-  // Create a new task by updating the global state
+  // Create a new task (create new object for array)
   tasks.push({
     taskName,
     pomodoroDone: 0,
@@ -22,47 +22,56 @@ const addTask = function(e) {
     finished: false
   });
 
-  // Render the global state
+  // Render the global state (Render layout)
   renderTasks(pomodoroTableBody, tasks);
 };
 
 pomodoroForm.addEventListener('submit', addTask);
 
-// -->
+// Render new Task's layout
 const renderTasks = (tBodyNode, tasks = []) => {
-  tBodyNode.innerHTML = tasks.map((task, id) =>
+  tBodyNode.innerHTML = tasks.map((currentTask, index) =>
   `<tr>
-      <td class="cell-task-name">${task.taskName}</td>
-      <td class="cell-pom-count">${task.pomodoroDone} / ${task.pomodoroCount} pomodori</td>
+      <td class="cell-task-name">${currentTask.taskName}</td>
+      <td class="cell-pom-count">${currentTask.pomodoroDone} / ${currentTask.pomodoroCount} pomodori</td>
       <td class="cell-pom-controls">
-      ${task.finished ? 'Finished' :
-					`<button class="js-task-done" data-id="${id}">Done</button>
-      		<button class="js-increase-pomodoro" data-id="${id}">Increase Pomodoro Count</button>`
+      ${currentTask.finished ? 'Finished' :
+					`<button class="js-task-done" data-id="${index}">Done</button>
+      		<button class="js-increase-pomodoro" data-id="${index}">Increase Pomodoro Count</button>`
 				}
-          <button class="js-delete-task" data-id="${id}">Delete Task</button>
+          <button class="js-delete-task" data-id="${index}">Delete Task</button>
       </td>
   </tr>`).join('');
 };
 
 // Implement event handlers
-const finishTask = ( tasks, taskId ) => {
-	tasks[taskId].finished = true;
+const finishTask = (tasksArray, taskId) => {
+	tasksArray[taskId].finished = true;
+
+	// Define the which type of the input parameter (Code Analysis)
 	console.log(taskId);
+	console.log(tasksArray);
+	console.log(tasksArray[taskId]);
 }
 
-const increasePomodoroDone = ( tasks, taskId ) => {
-	const limit = tasks[taskId].pomodoroCount;
-	if(tasks[taskId].pomodoroDone < limit) {
-		tasks[taskId].pomodoroDone += 1;
+// Implement event handlers
+const increasePomodoroDone = (tasksArray, taskId) => {
+	const limit = tasksArray[taskId].pomodoroCount;
+	if(tasksArray[taskId].pomodoroDone < limit) {
+		tasksArray[taskId].pomodoroDone += 1;
 		console.log(taskId);
 	}
 }
 
-const deleteTask = ( tasks, taskId ) => {
-	tasks.splice(taskId, 1);
+// Implement event handlers
+const deleteTask = (tasksArray, taskId) => {
+	tasksArray.splice(taskId, 1);
 	console.log(taskId);
 }
 
+/* Implement Big event to rule them all, One event handler to rule them all. When a click on a DOM node happens and an event handler is not defined on the node,
+the event handler defined on the closest parent node captures and handles the event.
+-> To handle event propagation without adding event listeners at runtime, I define Event for Table-Body-Node because it contains the buttons*/
 const handleTaskButtonClick = function(e) {
 	const taskId = e.target.getAttribute('data-id');
 
