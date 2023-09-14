4efa97e0b2b90891ef6fb590da917fe00b17fbfe
tantran <duytantran.it@gmail.com>
2023-09-14 10:28:38 +0700

    Update: create buttons Add to Cart and add comments

--- practices/js-practice-one/views/menu.view.js
+++ practices/js-practice-one/views/menu.view.js
@@ -2,15 +2,22 @@ import Observer from './observer.js';
 
 class MenuView extends Observer {
   constructor(controller) {
+    // super() method refer to Observer class, help MenuView class and access update() method of it's parent
     super();
+    // Pass parameter "controller" to property name to access controller's properties and methods()
     this.controller = controller;
     this.menuList = document.querySelector('.table-menu');
+    // Create Table body first because:
+    // The methods insertRow() takes care of creating a tBody only if it is called on an empty Table.
     this.generateTableBody(this.menuList, controller.model.menuList)
     this.generateTableHead(this.menuList);
-    console.log(this.menuList);
- 
+    // console.log methods below help me identify exactly objects in the DOM
+    // console.log(this.menuList);
+    // console.log(this.menuList.lastElementChild.children);
+    this.generateButton(this.menuList.lastElementChild.children);
   }
 
+  // Use DOM-appendChild to create table-Head in Table
   generateTableHead(table) {
     const thead = table.createTHead();
     const row = thead.insertRow();
@@ -20,6 +27,7 @@ class MenuView extends Observer {
     th.setAttribute('colspan', 4);
   }
 
+  // For each object in Array -> create table row for it. For each keyName in Object: create table data (insertCell) and display it's value
   generateTableBody(table, data) {
     for (const element of data) {
       let row = table.insertRow();
@@ -31,7 +39,21 @@ class MenuView extends Observer {
     }
   }
 
+  // Passed ***[HTML collection]: children of tBody: (lastElementChild of Table)*** into method and create button for each index in collection.
+  // In conclusion: I use this method to create button for each row in Menu
+  generateButton(tableRow) {
+    for(let index = 0; index < tableRow.length; index++) {
+      let button = document.createElement('button');
+      button.textContent = 'Add to Cart';
+      button.setAttribute('type', 'button');
+      tableRow[index].appendChild(button);
+      button.className = `menu-btn btn-${index+1}`;
+    }
+  }
+
+  // Updating later
   update() {}
 }
 
+// export class MenuView to import outside
 export { MenuView };
