71a6e55804b7bd4b67987c646069e2b5e40df504
tantran <duytantran.it@gmail.com>
2023-09-18 14:17:09 +0700

    Add events to button and update menu view, cart view

--- practices/js-practice-one/views/menu.view.js
+++ practices/js-practice-one/views/menu.view.js
@@ -6,20 +6,22 @@ class MenuView extends Observer {
     super();
     // Pass parameter "controller" to property name to access controller's properties and methods()
     this.controller = controller;
+    // Render Menu's View
+    this.render(this.controller.menuModel.menuList);
+
+    this.controller.menuModel.addObserver(this);
+  }
+
+  render(data) {
     this.menuList = document.querySelector('.table-menu');
     // Create Table body first because:
     // The methods insertRow() takes care of creating a tBody only if it is called on an empty Table.
-    this.generateTableBody(this.menuList, controller.model.menuList);
+    this.generateTableBody(this.menuList, data);
     this.generateTableHead(this.menuList);
     // console.log methods below help me identify exactly objects in the DOM
     // console.log(this.menuList);
     // console.log(this.menuList.lastElementChild.children);
     this.generateButton(this.menuList.lastElementChild.children);
-
-    this.menuButton = document.querySelectorAll('.menu-btn');
-    console.log(this.menuButton);
-    this.menuButton.addEventListener('click', controller, { once: true });
-    // this.menuButton.addEventListener('click', controller);
   }
 
   // Use DOM-appendChild to create table-Head in Table
@@ -37,27 +39,40 @@ class MenuView extends Observer {
     for (const element of data) {
       let row = table.insertRow();
       for (let key in element) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text);
+        if (key != 'id') {
+          let cell = row.insertCell();
+          let text = document.createTextNode(element[key]);
+          cell.appendChild(text);
+        }
       }
     }
   }
 
   // Passed ***[HTML collection]: children of tBody: (lastElementChild of Table)*** into method and create button for each index in collection.
   // In conclusion: I use this method to create button for each row in Menu
+  // Then add Event Listener for each button
   generateButton(tableRow) {
-    for(let index = 0; index < tableRow.length; index++) {
+    for (let index = 0; index < tableRow.length; index++) {
       let button = document.createElement('button');
       button.textContent = 'Add to Cart';
       button.setAttribute('type', 'button');
+      button.setAttribute('menuId', index);
       tableRow[index].appendChild(button);
-      button.className = `menu-btn btn-${index+1}`;
+      button.className = `menu-btn btn-${index}`;
+
+      // Add listener for each button by define menuId attribute (in handleEvent method of controller)
+      button.addEventListener('click', this.controller);
     }
   }
 
-  // Updating later
-  update() {}
+  // Update Menu View
+  update(data) {
+    // Remove thead and tbody node
+    this.menuList.removeChild(this.menuList.firstElementChild);
+    this.menuList.removeChild(this.menuList.lastElementChild);
+    // Re-render Thead and Tbody
+    this.render(data);
+  }
 }
 
 // export class MenuView to import outside
