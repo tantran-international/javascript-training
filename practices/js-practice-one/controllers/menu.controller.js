3ce9c2f6f9afeb36603f0af38bd8dfabf7c1d323
tantran <duytantran.it@gmail.com>
2023-09-18 14:18:35 +0700

    Handling click Event for menu view and cart view

--- practices/js-practice-one/controllers/menu.controller.js
+++ practices/js-practice-one/controllers/menu.controller.js
@@ -1,17 +1,43 @@
 class MenuController {
-  constructor(model) {
-  // Pass parameter "model" to property name to access model's properties in constructor() method
-  // MenuView can access model.menuList via this"controller".model.menuList
-    this.model = model;
+  constructor(menuModel, cartModel) {
+    // Pass parameter "model" to property name to access model's properties in constructor() method
+    // MenuView can access model.menuList via this"controller".model.menuList
+    this.menuModel = menuModel;
+    this.cartModel = cartModel;
   }
 
   handleEvent(e) {
     e.stopPropagation();
-    
+    // console.log(e.target.getAttribute('menuId'));
+    const menuId = e.target.getAttribute('menuId');
+    const item = this.menuModel.getItem(menuId);
+
+    switch (menuId) {
+      case '0':
+        this.menuModel.decrease(menuId);
+        this.cartModel.addItem(item);
+        console.log(item);
+        break;
+
+      case '1':
+        this.menuModel.decrease(menuId);
+        this.cartModel.addItem(item);
+        console.log(item);
+        break;
+
+      case '2':
+        this.menuModel.decrease(menuId);
+        this.cartModel.addItem(item);
+        console.log(item);
+        break;
+
+      default:
+        break;
+    }
   }
-  
+
   addtoCart(amount) {
-    
+
   }
   // get modelMenuTable() {
   //   return this.model.menuList;
