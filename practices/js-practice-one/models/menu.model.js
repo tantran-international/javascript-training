ae4501f76cf73a2ae332ce2aa15ab03d73b5f4ce
tantran <duytantran.it@gmail.com>
2023-09-18 14:18:01 +0700

    Update data and methods for Models

--- practices/js-practice-one/models/menu.model.js
+++ practices/js-practice-one/models/menu.model.js
@@ -4,11 +4,34 @@ class MenuModel extends Observable {
   constructor() {
     super();
     this.menuList = [
-      { name: 'Banh Beo', amount: 10, price: 1000 },
-      { name: 'Banh Loc', amount: 10, price: 2000 },
-      { name: 'Banh Nam', amount: 10, price: 3000 }
+      { id: 0, name: 'Banh Beo', amount: 10, price: 1000 },
+      { id: 1, name: 'Banh Loc', amount: 10, price: 2000 },
+      { id: 2, name: 'Banh Nam', amount: 10, price: 3000 }
     ];
   }
+
+  getItem(menuId) {
+    for (let index = 0; index < this.menuList.length; index++) {
+      const element = this.menuList[index];
+      if (element.id == menuId) {
+        return element;
+      }
+    }
+  }
+
+  decrease(menuId) {
+    for (let index = 0; index < this.menuList.length; index++) {
+      const element = this.menuList[index];
+      // console.log(element);
+      if (element.id == menuId) {
+        if (element.amount > 0) {
+          element.amount--;
+        }
+      }
+    }
+    this.notify(this.menuList);
+  }
+
 }
 
 export { MenuModel };
