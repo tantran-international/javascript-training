d7e7c7aca4142adf70adf7009bdf573d40768b0f
tantran <duytantran.it@gmail.com>
2023-09-19 14:42:11 +0700

    Update comment for payment view

--- practices/js-practice-one/views/payment.view.js
+++ practices/js-practice-one/views/payment.view.js
@@ -6,17 +6,18 @@ class PaymentView extends Observer {
     this.controller = controller;
     this.totalPayment = document.querySelector('.total-amount');
     // Display total amount of price to payment view
-    this.totalPayment.innerText = controller.paymentCost;
+    this.totalPayment.innerText = controller.paymentModel.totalPayment;
 
-    // Add Event
+    // Add Event for checkout button
     this.checkoutButton = document.querySelector('.btn-checkout');
     this.checkoutButton.addEventListener('click', this.controller);
 
+    // PaymentModel tracking
     this.controller.paymentModel.addObserver(this);
   }
 
-  update(model) {
-    this.totalPayment.innerText = model.totalPayment;
+  update(data) {
+    this.totalPayment.innerText = data;
   }
 }
 
