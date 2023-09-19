67bb643b5130f2349ab8779467b267a94366428d
Duy Tran <duytran@asnet.com.vn>
2023-09-19 11:24:01 +0700

    fix table

--- practices/js-practice-one/views/cart.view.js
+++ practices/js-practice-one/views/cart.view.js
@@ -1,68 +1,75 @@
 import Observer from "./observer.js";
 
 class CartView extends Observer {
-  constructor(controller) {
-    super();
-    this.controller = controller;
-    this.controller.cartModel.addObserver(this);
+	constructor(controller) {
+		super();
+		this.controller = controller;
+		this.controller.cartModel.addObserver(this);
 
-    this.render(this.controller.cartModel.cartList);
-  }
+		this.render(this.controller.cartModel.cartList);
+	}
 
-  render(data) {
-    // this.cartList = document.querySelector('.table-cart');
-    // this.generateTableBody(this.cartList, data);
-    // this.generateTableHead(this.cartList);
-    
-    // if (this.cartList.length != 0) {
-    //   this.cartList.removeChild(this.cartList.firstElementChild);
-    //   this.cartList.removeChild(this.cartList.lastElementChild);
-    //   console.log(this.cartList.lastElementChild);
-    //   this.generateTableBody(this.cartList, data);
-    //   this.generateTableHead(this.cartList);
-    // }
-    // console.log(this.cartList);
+	render(data) {
+		// this.cartList = document.querySelector('.table-cart');
+		// this.generateTableBody(this.cartList, data);
+		// this.generateTableHead(this.cartList);
 
+		// if (this.cartList.length != 0) {
+		//   this.cartList.removeChild(this.cartList.firstElementChild);
+		//   this.cartList.removeChild(this.cartList.lastElementChild);
+		//   console.log(this.cartList.lastElementChild);
+		//   this.generateTableBody(this.cartList, data);
+		//   this.generateTableHead(this.cartList);
+		// }
+		// console.log(this.cartList);
 
-    this.cartList = document.querySelector('.table-cart');
-    // Remove all children of table
+		// this.cartList = document.querySelector('.table-cart');
+		// // Remove all children of table
 
-    if (data.length !== 0) {
-      this.cartList.removeChild(this.cartList.firstChild);
-    }
+		// if (data.length !== 0) {
+		//   this.cartList.removeChild(this.cartList.firstChild);
+		// }
 
-    // Render Table Header
-    this.generateTableHead(this.cartList);
+		// // Render Table Header
+		// this.generateTableHead(this.cartList);
 
-  }
+		if (data.lenght !== 0) {
+			this.cartList = document.querySelector(".table-cart");
+			// remove toan bo child table-cart
+			this.cartList.innerHTML = "";
 
-  generateTableHead(table) {
-    const thead = table.createTHead();
-    const row = thead.insertRow();
-    const th = document.createElement('th');
-    th.innerText = 'Menu';
-    row.appendChild(th);
-    th.setAttribute('colspan', 3);
-  }
+			this.generateTableHead(this.cartList);
+			this.generateTableBody(this.cartList, data);
+		}
+	}
 
-  generateTableBody(table, data) {
-    for (const element of data) {
-      let row = table.insertRow();
-      for (const key in element) {
-        if (key == 'name' || key == 'amount') {
-          let cell = row.insertCell();
-          let text = document.createTextNode(element[key]);
-          cell.appendChild(text);
-        }
-      }
-    }
-  }
+	generateTableHead(table) {
+		const thead = table.createTHead();
+		const row = thead.insertRow();
+		const th = document.createElement("th");
+		th.innerText = "Menu";
+		row.appendChild(th);
+		th.setAttribute("colspan", 3);
+	}
 
-  update(data) {
-    // console.log(this.cartList);
+	generateTableBody(table, data) {
+		for (const element of data) {
+			let row = table.insertRow();
+			for (const key in element) {
+				if (key == "name" || key == "amount") {
+					let cell = row.insertCell();
+					let text = document.createTextNode(element[key]);
+					cell.appendChild(text);
+				}
+			}
+		}
+	}
 
-    this.render(data);
-  }
+	update(data) {
+		// console.log(this.cartList);
+
+		this.render(data);
+	}
 }
 
 export { CartView };
