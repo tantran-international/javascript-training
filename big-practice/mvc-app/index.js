41f1307eb43adbfd2fc7a76af0a6780a03ef8018
tantran <duytantran.it@gmail.com>
2023-09-28 19:29:52 +0700

    Update modalUpdateView for main function

--- big-practice/mvc-app/index.js
+++ big-practice/mvc-app/index.js
@@ -1,17 +1,20 @@
 import { TaskModel } from "./models/task.model.js";
-import { TaskController } from "./controllers/task.controller.js";
+import { TaskController } from "./controllers/task.controllers.js";
 import { TaskView } from "./views/task.view.js";
 import { TodoView } from "./views/todo.view.js";
 import { InprogressView } from "./views/inprogress.view.js";
 import { DoneView } from "./views/done.view.js";
+import { ModalUpdateView } from "./views/modal.update.view.js";
 
 function main() {
   let taskModel = new TaskModel();
   let taskController = new TaskController(taskModel);
   let taskView = new TaskView(taskController);
-  let todoView = new TodoView(taskController, taskView);
-  let inprogressView = new InprogressView(taskController, taskView);
-  let doneView = new DoneView(taskController, taskView);
+  let modalUpdateView = new ModalUpdateView(taskController);
+  let todoView = new TodoView(taskController, taskView, modalUpdateView, modalUpdateView);
+  let inprogressView = new InprogressView(taskController, taskView, modalUpdateView);
+  let doneView = new DoneView(taskController, taskView, modalUpdateView);
+
 }
 
 main();
