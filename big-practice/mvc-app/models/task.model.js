b252d0bf80af6e82656668faf008fc711b194007
tantran <duytantran.it@gmail.com>
2023-09-28 19:30:29 +0700

    Update status for sample data

--- big-practice/mvc-app/models/task.model.js
+++ big-practice/mvc-app/models/task.model.js
@@ -9,19 +9,33 @@ class TaskModel extends Observable {
         { id: 1, title: 'Banh Loc', createAt: 10, updateAt: '10gio', status: 'todo' },
         { id: 2, title: 'Banh Nam', createAt: 10, updateAt: '10gio', status: 'todo' }
       ],
-      inprogress: [
-        { id: 0, title: 'Banh Beo', createAt: '9am', updateAt: '10gio', status: 'todo' },
-        { id: 1, title: 'Banh Loc', createAt: 9, updateAt: '10gio', status: 'todo' },
-        { id: 2, title: 'Banh Nam', createAt: 9, updateAt: '10gio', status: 'todo' }
+      inprogresss: [
+        { id: 0, title: 'Banh Beo', createAt: '9am', updateAt: '10gio', status: 'inprogress' },
+        { id: 1, title: 'Banh Loc', createAt: 9, updateAt: '10gio', status: 'inprogress' },
+        { id: 2, title: 'Banh Nam', createAt: 9, updateAt: '10gio', status: 'inprogress' }
       ],
       dones: [
-        { id: 0, title: 'Banh Beo', createAt: 888, updateAt: '10gio', status: 'todo' },
-        { id: 1, title: 'Banh Loc', createAt: 8, updateAt: '10gio', status: 'todo' },
-        { id: 2, title: 'Banh Nam', createAt: 8, updateAt: '10gio', status: 'todo' }
+        { id: 0, title: 'Banh Beo', createAt: 888, updateAt: '10gio', status: 'done' },
+        { id: 1, title: 'Banh Loc', createAt: 8, updateAt: '10gio', status: 'done' },
+        { id: 2, title: 'Banh Nam', createAt: 8, updateAt: '10gio', status: 'done' }
       ]
     }
   }
 
+
+  getItem(dataId, dataStatus) {
+    for (let key in this.tasks) {
+      if (dataStatus == key) {
+        const dataList = this.tasks[key];
+        for (let index = 0; index < dataList.length; index++) {
+          const element = dataList[index];
+          if (dataId == dataList[index].id) {
+            return dataList[index];
+          }
+        }
+      }
+    }
+  }
 }
 
 export { TaskModel };
